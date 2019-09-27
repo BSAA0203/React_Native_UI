7a3629148d724c4dddc0b4732a09a6761d3ae84a
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-09-27 16:13:01 +0900

    Desgin icon & page animation

--- React_Native_UI/Components/MainScreen.js
+++ React_Native_UI/Components/MainScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View,Text,StyleSheet, Platform } from 'react-native';
 import { Icon } from 'native-base';
 import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
 
 import HomeTab from './AppTabNavigator/HomeTab'
 import SearchTab from './AppTabNavigator/SearchTab'
@@ -10,12 +10,33 @@ import AddMediaTab from './AppTabNavigator/AddMediaTab'
 import LikesTab from './AppTabNavigator/LikesTab'
 import ProfileTab from './AppTabNavigator/ProfileTab'
 
-const AppTabNavigator = createBottomTabNavigator({
+const AppTabNavigator = createMaterialTopTabNavigator({
   HomeTab: { screen: HomeTab },
   SearchTab: { screen: SearchTab },
   AddMediaTab: { screen: AddMediaTab },
   LikesTab: { screen: LikesTab },
   ProfileTab: { screen: ProfileTab }
+},{
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        ios:{
+          backgroundColor:'white',
+        },
+        android:{
+          backgroundColor:'white',
+        }
+      })
+    },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: false,
+    showLabel: false,
+    showIcon: true,
+  }
 });
 
 const AppTabContainet = createAppContainer(AppTabNavigator);
